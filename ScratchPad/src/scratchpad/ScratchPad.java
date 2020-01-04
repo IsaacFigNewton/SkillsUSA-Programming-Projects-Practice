c7911d434917a7402c8090508de38e7b517c7dea
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-01-04 08:15:52 -0800

    I forgot to push to master

---
+++ ScratchPad/src/scratchpad/ScratchPad.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scratchpad;
+
+/**
+ *
+ * @author Owner
+ */
+public class ScratchPad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Test FirstTest;
+        FirstTest.Test1(1);
+        //Test2(2);
+    }
+    
+}
---
+++ ScratchPad/src/scratchpad/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scratchpad;
+
+/**
+ *
+ * @author Owner
+ */
+public class Test {
+    public void Test1(int input) {
+        System.out.println("Output 1: " + input);
+    }
+    public void Test2(int input) {
+        System.out.println("Output 2: " + input);
+    }
+}
